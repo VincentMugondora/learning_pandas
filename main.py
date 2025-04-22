96dccb59c2521e0a956c47acb8775d3ae9e1ebc6
vincentmugondora <vinmugondora@gmail.com>
2025-04-22 14:42:42 +0200

    Add initial CSV data file and update main.py to read and display DataFrame

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 import pandas as pd
+import numpy as np
 
 # Pandas is a Python module for working with tabular data (i.e., data in a table with rows and columns). Tabular data has a lot of the same functionality as SQL or Excel, but Pandas adds the power of Python.
+
