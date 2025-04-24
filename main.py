bcad1542040c1ec5b3fbe6e1aafbb95b6cafba74
vincentmugondora <vinmugondora@gmail.com>
2025-04-24 12:47:07 +0200

    Add list and dictionary definitions for data manipulation in main.py

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ import numpy as np
 
 # Pandas is a Python module for working with tabular data (i.e., data in a table with rows and columns). Tabular data has a lot of the same functionality as SQL or Excel, but Pandas adds the power of Python.
 
+l1 = [2,5]
+
+d1 = {'a': [1, 2, 3], 'b': [4, 5, 6], 'c': [7, 8, 9]} # rename index
